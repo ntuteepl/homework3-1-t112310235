8f69f9a73e532397d60bd8294ebb36a2e4b9b557
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-18 12:35:18 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+ main() {
+    int num;
+
+    scanf("%d", &num);
+    bool prime=true;
+
+    for (int i = 2; i * i <= num; i++){
+        if ((num % i) == 0){
+           prime=false;
+
+
+      }
+
+    }
+    if(prime==1)
+        printf("YES");
+        if(prime==0)
+        printf("NO");
+ }
