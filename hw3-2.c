723d8d89f01cd57b04a6ff4cad6fab3d3a51f400
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-18 16:08:05 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -2,7 +2,7 @@
 
 int main() {
     int s1, d1, s2, d2, s3, d3;
-   
+
     scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
     if (s1>s2){
         int temp=s1;
@@ -34,11 +34,32 @@ int main() {
 
 
     }
+     if (d1>d2){
+        int temp=d1;
+           d1=d2;
+            d2=temp;
+
+
+    }
+     if (d2>d3){
+        int temp=d2;
+           d2=d3;
+            d3=temp;
+
+
+    }
+     if (d1>d2){
+        int temp=d1;
+           d1=d2;
+            d2=temp;
+
+
+    }
 
     int cars=1;
-    if(d1>=s2&&d1<=s3)
+    if(d1>s2&&d1<=s3)
         cars=2;
-    if(d1>=s2&&d1>s3)
+    if(d1>s2&&d1>s3)
         cars=3;
 
     printf(" %d\n", cars);
