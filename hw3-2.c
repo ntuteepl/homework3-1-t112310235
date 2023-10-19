e0bd2fc0c5c824f75d72d0f4aee268831af15002
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-19 12:33:12 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,51 +1,54 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void sort(int start[], int end[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (end[j] > end[j + 1]) {
-                swap(&end[j], &end[j + 1]);
-                swap(&start[j], &start[j + 1]);
-            }
-        }
-    }
-}
-
 int main() {
-    int start[3], end[3];
+    int start1, end1, start2, end2, start3, end3;
 
     // 输入订单的起始时间和结束时间
-    for (int i = 0; i < 3; i++) {
-        scanf("%d %d", &start[i], &end[i]);
+    scanf("%d %d %d %d %d %d", &start1, &end1, &start2, &end2, &start3, &end3);
+
+    // 根据订单的结束时间手动排序
+    if (end1 > end2) {
+        int temp = end1;
+        end1 = end2;
+        end2 = temp;
+        temp = start1;
+        start1 = start2;
+        start2 = temp;
     }
-    
-    // 按结束时间对订单进行排序
-    sort(start, end, 3);
+
+    if (end2 > end3) {
+        int temp = end2;
+        end2 = end3;
+        end3 = temp;
+        temp = start2;
+        start2 = start3;
+        start3 = temp;
+    }
+
+    if (end1 > end2) {
+        int temp = end1;
+        end1 = end2;
+        end2 = temp;
+        temp = start1;
+        start1 = start2;
+        start2 = temp;
+    }
 
     int cars = 1;
-    int current_end = end[0];
+    int current_end = end1;
 
-    for (int i = 1; i < 3; i++) {
-        if (start[i] >= current_end) {
-            // 当前订单可以使用同一辆游览车
-            current_end = end[i];
-        } else {
-            // 需要另一辆游览车
-            cars++;
-            current_end = end[i];
-        }
+    if (start2 >= current_end) {
+        current_end = end2;
+    } else {
+        cars++;
+        current_end = end3;
+    }
+
+    if (start3 >= current_end) {
+        current_end = end3;
     }
 
     printf("%d\n", cars);
 
     return 0;
 }
-
-
-
