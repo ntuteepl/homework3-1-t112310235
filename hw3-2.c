d9039e2a0625a2f823b0891d5536ef315859f459
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-21 16:19:18 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -6,8 +6,8 @@ int main() {
     // 输入订单的起始时间和结束时间
     scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
 
-    // 将订单按出发时间升序排序
-    if (s1 > s2) {
+    // 对订单按出发时间升序，结束时间降序排序
+    if (s1 > s2 || (s1 == s2 && d1 < d2)) {
         int temp = s1;
         s1 = s2;
         s2 = temp;
@@ -16,7 +16,7 @@ int main() {
         d2 = temp;
     }
 
-    if (s2 > s3) {
+    if (s2 > s3 || (s2 == s3 && d2 < d3)) {
         int temp = s2;
         s2 = s3;
         s3 = temp;
@@ -25,7 +25,7 @@ int main() {
         d3 = temp;
     }
 
-    if (s1 > s2) {
+    if (s1 > s2 || (s1 == s2 && d1 < d2)) {
         int temp = s1;
         s1 = s2;
         s2 = temp;
@@ -36,17 +36,9 @@ int main() {
 
     int cars = 1; // 初始化车辆数量为1
 
-    // 遍历订单，检查时间表冲突
-    if (s2 < d1) {
-        cars++; // 需要额外的车辆
-    }
-    if (s3 < d2) {
-        cars++; // 需要额外的车辆
-    }
-    if(s3>=d1){
-        cars--;
-    }
-
+    // 循环遍历订单，如果下一个订单的出发时间大于前一个订单的结束时间，增加车辆
+    if (s2 > d1) cars++;
+    if (s3 > d2) cars++;
 
     printf("%d\n", cars); // 输出最少车辆数量
 
