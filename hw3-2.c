19e3967a73b6c34a913bfd08a638717174c38340
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-21 16:35:47 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -6,41 +6,42 @@ int main() {
     // 输入订单的起始时间和结束时间
     scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
 
+    // 对订单按出发时间升序，结束时间降序排序
+    if (s1 > s2 || (s1 == s2 && d1 < d2)) {
+        int temp = s1;
+        s1 = s2;
+        s2 = temp;
+        temp = d1;
+        d1 = d2;
+        d2 = temp;
+    }
+
+    if (s2 > s3 || (s2 == s3 && d2 < d3)) {
+        int temp = s2;
+        s2 = s3;
+        s3 = temp;
+        temp = d2;
+        d2 = d3;
+        d3 = temp;
+    }
+
+    if (s1 > s2 || (s1 == s2 && d1 < d2)) {
+        int temp = s1;
+        s1 = s2;
+        s2 = temp;
+        temp = d1;
+        d1 = d2;
+        d2 = temp;
+    }
+
     int cars = 1; // 初始化车辆数量为1
 
-    // 找到最晚的订单开始时间
-    int latest_start = s1;
-    if (s2 > latest_start) {
-        latest_start = s2;
-    }
-    if (s3 > latest_start) {
-        latest_start = s3;
-    }
-
-    // 找到最早的订单结束时间
-    int earliest_end = d1;
-    if (d2 < earliest_end) {
-        earliest_end = d2;
-    }
-    if (d3 < earliest_end) {
-        earliest_end = d3;
-    }
-
-    if (latest_start >= earliest_end) {
-        cars = 0;  // 如果没有订单重叠，不需要额外的车辆
-    } else {
-        if (s1 <= latest_start && d1 > latest_start) {
-            cars++;
-        }
-        if (s2 <= latest_start && d2 > latest_start) {
-            cars++;
-        }
-        if (s3 <= latest_start && d3 > latest_start) {
-            cars++;
-        }
-    }
+    // 循环遍历订单，如果下一个订单的出发时间大于前一个订单的结束时间，增加车辆
+    if (s2 > d1) cars++;
+    if (s3 > d2) cars++;
 
     printf("%d\n", cars); // 输出最少车辆数量
 
     return 0;
 }
+
