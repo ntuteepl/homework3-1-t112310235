b61c00a4eb7e26b0e3d7b4a80668012354b1aaca
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-21 15:23:50 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
 
 int main() {
-    int start1, end1, start2, end2, start3, end3;
+    int s1, d1, s2, d2, s3, d3;
 
     // 输入订单的起始时间和结束时间
-    scanf("%d %d %d %d %d %d", &start1, &end1, &start2, &end2, &start3, &end3);
+    scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
 
     // 根据订单的结束时间手动排序
-    if (start1 > start2) {
-        int temp = end1;
-        end1 = end2;
-        end2 = temp;
-        temp = start1;
-        start1 = start2;
-        start2 = temp;
+    if (s1 > s2) {
+        int temp = d1;
+        d1 = d2;
+        d2 = temp;
+        temp = s1;
+        s1 = s2;
+        s2 = temp;
     }
 
-    if (start2 > start3) {
-        int temp = end2;
-        end2 = end3;
-        end3 = temp;
-        temp = start2;
-        start2 = start3;
-        start3 = temp;
+    if (s2 > s3) {
+        int temp = d2;
+        d2 = d3;
+        d3 = temp;
+        temp = s2;
+        s2 = s3;
+        s3 = temp;
     }
 
-    if (start1 > start2) {
-        int temp = end1;
-        end1 = end2;
-        end2 = temp;
-        temp = start1;
-        start1 = start2;
-        start2 = temp;
+    if (s1 > s2) {
+        int temp = d1;
+        d1 = d2;
+        d2 = temp;
+        temp = s1;
+        s1 = s2;
+        s2 = temp;
     }
 
     int cars = 1;
-    if((end1 > start2 && end1 <= start3)||(end1 <= start2 && end2 > start3)||(end1 > start2 && start3 > end2 && start3 > end1))
-        cars=2;
-    if((end1> start2 && start3 > start2 && start3 < start1)||(start1 == start2 && start3> start1 && start3> start2))
-        cars=3;
-    printf("%d\n", cars);
+
+    if ((d1>s2&&s3>=s1)|| (d1<=s2&&s3<d2)||(d1>s2&&s3>=s2))
+        cars = 2 ;
+    if((d1>s2&&s3>s2&&s3<s1)||(s1==s2&&s3<d1&&s3<d2)||(s2==s3&&d1>s2))
+         cars = 3 ;
+        printf("%d\n", cars);
 
     return 0;
 }
