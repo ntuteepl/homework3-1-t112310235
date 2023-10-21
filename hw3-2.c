0f767e128964c2d57c6a7a50d8b24d8db7f90692
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-21 16:05:34 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -6,27 +6,45 @@ int main() {
     // 输入订单的起始时间和结束时间
     scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
 
-    int cars = 0;
+    // 将订单按出发时间升序排序
+    if (s1 > s2) {
+        int temp = s1;
+        s1 = s2;
+        s2 = temp;
+        temp = d1;
+        d1 = d2;
+        d2 = temp;
+    }
 
-    // 首先考虑第一辆车
-    cars++;
+    if (s2 > s3) {
+        int temp = s2;
+        s2 = s3;
+        s3 = temp;
+        temp = d2;
+        d2 = d3;
+        d3 = temp;
+    }
 
-    // 检查第二辆车是否需要
+    if (s1 > s2) {
+        int temp = s1;
+        s1 = s2;
+        s2 = temp;
+        temp = d1;
+        d1 = d2;
+        d2 = temp;
+    }
+
+    int cars = 1; // 初始化车辆数量为1
+
+    // 遍历订单，检查时间表冲突
     if (s2 < d1) {
-        // 第二辆车的出发时间早于第一辆车的返回时间
-        cars++;
-    } else if (s2 < d2) {
-        // 第二辆车的出发时间早于第二个订单的结束时间
-        d1 = d2; // 更新第一辆车的结束时间为第二个订单的结束时间
+        cars++; // 需要额外的车辆
+    }
+    if (s3 < d2) {
+        cars++; // 需要额外的车辆
     }
 
-    // 检查第三辆车是否需要
-    if (s3 < d1) {
-        // 第三辆车的出发时间早于第一辆车的返回时间
-        cars++;
-    }
-
-    printf("%d\n", cars);
+    printf("%d\n", cars); // 输出最少车辆数量
 
     return 0;
 }
