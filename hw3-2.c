377249976c3dec48cb72b04ac9b49202363f64fb
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-21 15:54:00 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -6,35 +6,10 @@ int main() {
     // 输入订单的起始时间和结束时间
     scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
 
-    // 根据订单的结束时间手动排序
-    if (s1 > s2) {
-        int temp = d1;
-        d1 = d2;
-        d2 = temp;
-        temp = s1;
-        s1 = s2;
-        s2 = temp;
-    }
+    int cars = 0;
 
-    if (s2 > s3) {
-        int temp = d2;
-        d2 = d3;
-        d3 = temp;
-        temp = s2;
-        s2 = s3;
-        s3 = temp;
-    }
-
-    if (s1 > s2) {
-        int temp = d1;
-        d1 = d2;
-        d2 = temp;
-        temp = s1;
-        s1 = s2;
-        s2 = temp;
-    }
-
-    int cars = 1;
+    // 首先考虑第一辆车
+    cars++;
 
     // 检查第二辆车是否需要
     if (s2 < d1) {
