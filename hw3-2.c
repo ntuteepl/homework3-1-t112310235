d0ffcc6a7972376188762305a43792d1e387c5c2
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-21 16:52:32 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -3,38 +3,45 @@
 int main() {
     int s1, d1, s2, d2, s3, d3;
 
-    // 输入订单的起始时间和结束时间
+    // 输入订单的出发时间和返回时间
     scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
 
-    // 对订单按出发时间升序排序
-    if (s1 > s2) {
-        int temp = s1;
-        s1 = s2;
-        s2 = temp;
-        temp = d1;
-        d1 = d2;
-        d2 = temp;
+    int max_return_time = 0; // 初始化最晚的返回时间为0
+
+    // 找出最晚的返回时间
+    if (d1 > max_return_time) {
+        max_return_time = d1;
     }
-    if (s2 > s3) {
-        int temp = s2;
-        s2 = s3;
-        s3 = temp;
-        temp = d2;
-        d2 = d3;
-        d3 = temp;
+    if (d2 > max_return_time) {
+        max_return_time = d2;
     }
-    if (s1 > s2) {
-        int temp = s1;
-        s1 = s2;
-        s2 = temp;
-        temp = d1;
-        d1 = d2;
-        d2 = temp;
+    if (d3 > max_return_time) {
+        max_return_time = d3;
     }
 
-    int cars = 1; // 初始化车辆数量为1
-   
-   
+    int cars = 0; // 初始化车辆数量为0
+    int current_time = 0; // 初始化当前时间为0
+
+    // 循环遍历时间，如果当前时间小于最晚的返回时间，增加车辆并更新当前时间
+    while (current_time < max_return_time) {
+        int earliest_next_start = max_return_time + 1; // 初始化下一个订单的最早出发时间为比最晚返回时间更晚的时间
+
+        // 寻找下一个订单的最早出发时间
+        if (s1 > current_time && s1 < earliest_next_start) {
+            earliest_next_start = s1;
+        }
+        if (s2 > current_time && s2 < earliest_next_start) {
+            earliest_next_start = s2;
+        }
+        if (s3 > current_time && s3 < earliest_next_start) {
+            earliest_next_start = s3;
+        }
+
+        // 更新当前时间为下一个订单的最早出发时间
+        current_time = earliest_next_start;
+        cars++; // 增加车辆数量
+    }
+
     printf("%d\n", cars); // 输出最少车辆数量
 
     return 0;
