ac79d935a5673b72ed3d7042e3973bfe10e437fe
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-21 16:24:16 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -6,41 +6,40 @@ int main() {
     // 输入订单的起始时间和结束时间
     scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
 
-    // 对订单按出发时间升序，结束时间降序排序
-    if (s1 > s2 || (s1 == s2 && d1 > d2)) {
-        int temp = s1;
-        s1 = s2;
-        s2 = temp;
-        temp = d1;
-        d1 = d2;
-        d2 = temp;
+    // 找出最晚的结束时间
+    int latest_end_time = d1;
+    if (d2 > latest_end_time) {
+        latest_end_time = d2;
+    }
+    if (d3 > latest_end_time) {
+        latest_end_time = d3;
     }
 
-    if (s2 > s3 || (s2 == s3 && d2 > d3)) {
-        int temp = s2;
-        s2 = s3;
-        s3 = temp;
-        temp = d2;
-        d2 = d3;
-        d3 = temp;
+    int cars_needed = 0;  // 用于跟踪需要的车辆数量
+    int current_time = s1;  // 用于模拟时间线
+
+    while (current_time <= latest_end_time) {
+        int order_overlap = 0;  // 用于标记当前时间是否有订单需求
+
+        if (s1 <= current_time && d1 >= current_time) {
+            order_overlap = 1;
+        }
+        if (s2 <= current_time && d2 >= current_time) {
+            order_overlap = 1;
+        }
+        if (s3 <= current_time && d3 >= current_time) {
+            order_overlap = 1;
+        }
+
+        if (order_overlap) {
+            current_time++;
+        } else {
+            current_time = s1;  // 没有订单需求时，重置时间线到第一个订单的出发时间
+            cars_needed++;  // 分配一辆车
+        }
     }
 
-    if (s1 > s2 || (s1 == s2 && d1 > d2)) {
-        int temp = s1;
-        s1 = s2;
-        s2 = temp;
-        temp = d1;
-        d1 = d2;
-        d2 = temp;
-    }
-
-    int cars = 1; // 初始化车辆数量为1
-
-    // 循环遍历订单，如果下一个订单的出发时间大于前一个订单的结束时间，增加车辆
-    if (s2 > d1) cars++;
-    if (s3 > d2) cars++;
-
-    printf("%d\n", cars); // 输出最少车辆数量
+    printf("%d\n", cars_needed); // 输出最少车辆数量
 
     return 0;
 }
