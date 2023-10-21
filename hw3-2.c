3d74653bf002b637b4eab3fcb191325f5122ca12
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-21 18:44:33 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -36,7 +36,7 @@ int main() {
     }
 
     int cars ; // 初始化车辆数量为1
-    if(d1<=s2&&d2<=s3)
+    if(s2>=d1&&s3>=d2)
         cars=1;
 
     if((s2>=d1&&d2>s3&&s3>d1)||(s3>=d1&&d1>s2&&d2>=s1)||(s3>=d2&&s2>=s1&&d1>d2&&d1>s3))
