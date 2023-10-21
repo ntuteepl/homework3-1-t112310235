8c2a822d9c8169cb4b26ba7a8ea468777852011d
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-21 16:30:23 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -6,39 +6,44 @@ int main() {
     // 输入订单的起始时间和结束时间
     scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
 
-    // 对订单按出发时间升序，结束时间降序排序
-    if (s1 > s2 || (s1 == s2 && d1 < d2)) {
-        int temp = s1;
-        s1 = s2;
-        s2 = temp;
-        temp = d1;
-        d1 = d2;
-        d2 = temp;
-    }
-
-    if (s2 > s3 || (s2 == s3 && d2 < d3)) {
-        int temp = s2;
-        s2 = s3;
-        s3 = temp;
-        temp = d2;
-        d2 = d3;
-        d3 = temp;
-    }
-
-    if (s1 > s2 || (s1 == s2 && d1 < d2)) {
-        int temp = s1;
-        s1 = s2;
-        s2 = temp;
-        temp = d1;
-        d1 = d2;
-        d2 = temp;
-    }
-
     int cars = 1; // 初始化车辆数量为1
 
-    // 循环遍历订单，如果下一个订单的出发时间大于前一个订单的结束时间，增加车辆
-    if (s2 > d1) cars++;
-    if (s3 > d2) cars++;
+    // 找到最早的订单开始时间
+    int earliest_start = s1;
+    if (s2 < earliest_start) {
+        earliest_start = s2;
+    }
+    if (s3 < earliest_start) {
+        earliest_start = s3;
+    }
+
+    // 找到最晚的订单结束时间
+    int latest_end = d1;
+    if (d2 > latest_end) {
+        latest_end = d2;
+    }
+    if (d3 > latest_end) {
+        latest_end = d3;
+    }
+
+    int timeline[25] = {0}; // 用于跟踪时间线上的订单需求
+
+    for (int t = earliest_start; t < latest_end; t++) {
+        if (t >= s1 && t < d1) {
+            timeline[t]++;
+        }
+        if (t >= s2 && t < d2) {
+            timeline[t]++;
+        }
+        if (t >= s3 && t < d3) {
+            timeline[t]++;
+        }
+
+        // 如果某个时间点的订单需求大于1，增加车辆
+        if (timeline[t] > 1) {
+            cars++;
+        }
+    }
 
     printf("%d\n", cars); // 输出最少车辆数量
 
