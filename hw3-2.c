713a926bd06239576b47e35d4ade3b40cb7ddb48
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-21 16:32:52 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -8,39 +8,34 @@ int main() {
 
     int cars = 1; // 初始化车辆数量为1
 
-    // 找到最早的订单开始时间
-    int earliest_start = s1;
-    if (s2 < earliest_start) {
-        earliest_start = s2;
+    // 找到最晚的订单开始时间
+    int latest_start = s1;
+    if (s2 > latest_start) {
+        latest_start = s2;
     }
-    if (s3 < earliest_start) {
-        earliest_start = s3;
-    }
-
-    // 找到最晚的订单结束时间
-    int latest_end = d1;
-    if (d2 > latest_end) {
-        latest_end = d2;
-    }
-    if (d3 > latest_end) {
-        latest_end = d3;
+    if (s3 > latest_start) {
+        latest_start = s3;
     }
 
-    int timeline[25] = {0}; // 用于跟踪时间线上的订单需求
+    // 找到最早的订单结束时间
+    int earliest_end = d1;
+    if (d2 < earliest_end) {
+        earliest_end = d2;
+    }
+    if (d3 < earliest_end) {
+        earliest_end = d3;
+    }
 
-    for (int t = earliest_start; t < latest_end; t++) {
-        if (t >= s1 && t < d1) {
-            timeline[t]++;
+    if (latest_start >= earliest_end) {
+        cars = 0;  // 如果没有订单重叠，不需要额外的车辆
+    } else {
+        if (s1 <= latest_start && d1 > latest_start) {
+            cars++;
         }
-        if (t >= s2 && t < d2) {
-            timeline[t]++;
+        if (s2 <= latest_start && d2 > latest_start) {
+            cars++;
         }
-        if (t >= s3 && t < d3) {
-            timeline[t]++;
-        }
-
-        // 如果某个时间点的订单需求大于1，增加车辆
-        if (timeline[t] > 1) {
+        if (s3 <= latest_start && d3 > latest_start) {
             cars++;
         }
     }
